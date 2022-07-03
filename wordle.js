6052de1139f2a46429ba604b5d1117dab794b7c0
Sai Deepthi Majji <deepthi2897@gmail.com>
2022-07-03 22:37:19 +0530

    Adding dictionary support till letter H
    How to add for remaining letters:
    
    Open DZ.json file and add
    var letterZ = [     //at start
    ] - //at end
    
    do this for all files from H to Z.
    
    Also add this entry in wordle.html and dictionary file in wordle.js

--- wordle.js
+++ wordle.js
@@ -5,6 +5,18 @@ var col=0;
 var gameOver=false;
 var word="SMILE";
 
+var dictionary = {
+    "A" : letterA, 
+    "B": letterB,
+    "C": letterC,
+    "D": letterD,
+    "E": letterE,
+    "F": letterF,
+    "G": letterG,
+    "H": letterH
+};
+
+
 window.onload=function(){
     initialize();
 }
@@ -48,20 +60,22 @@ function initialize(){
 
             else if (e.code=="Enter"){
                 update();
-                row++;
-                col=0;
             }
 
             if(!gameOver && row == height)
             {
                 gameOver = true;
                 document.getElementById("answer").innerText = word;
+                document.getElementById("answer").classList.remove("error");
             }
         })
 
         }
 
         function update(){
+            var guess = getGuess().toLocaleUpperCase();
+            var validword = dictionary[guess.charAt(0)][0][guess];
+            if(validword) {
             let correct=0;
             for(let c = 0; c < width; c++){
                 let currTile = document.getElementById(row.toString() + '-' + c.toString());
@@ -85,4 +99,20 @@ function initialize(){
                     gameOver = true;
                 }
             }
+
+            row++;
+            col=0;
+            document.getElementById("answer").innerText = "";
+            } else {
+                 document.getElementById("answer").innerText = "OOPS! Word doesn't exist in dictionary. Try again";
+                 document.getElementById("answer").classList.add("error");
+            }
+        }
+
+        function getGuess() {
+            var guess = "";
+            for (let i = 0; i < width; i++) {
+                guess = guess + document.getElementById(row.toString() + '-' + i.toString()).innerText;
+            }
+            return guess.toLocaleLowerCase();
         }
