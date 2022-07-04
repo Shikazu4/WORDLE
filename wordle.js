a50093c0ef52c841354fffdb6b7fdfb064735388
LazyShikazu <107610719+LazyShikazu@users.noreply.github.com>
2022-07-04 21:01:14 +0530

    format

--- wordle.js
+++ wordle.js
@@ -56,35 +56,35 @@ function initialize() {
 
     //keyboard
     let keyboard = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-                    ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-                    ["Enter", "Z", "X", "C", "V", "B", "N", "M", "⌫"]];
+    ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+    ["Enter", "Z", "X", "C", "V", "B", "N", "M", "⌫"]];
 
-    for(let i=0; i<3;i++){
+    for (let i = 0; i < 3; i++) {
         let currRow = keyboard[i];
         let kbrow = document.createElement("div");
         kbrow.classList.add("keyboardRow");
 
-        for(let j=0; j< currRow.length; j++){
+        for (let j = 0; j < currRow.length; j++) {
             let kTile = document.createElement("div");
             let key = currRow[j];
-            kTile.innerText= key;
+            kTile.innerText = key;
 
-            if(key == "Enter"){
+            if (key == "Enter") {
                 kTile.id = "Enter";
 
             }
             else if (key == "⌫")
                 kTile.id = "Backspace";
-            else if ("A" <= key && key <= "Z"){
+            else if ("A" <= key && key <= "Z") {
                 kTile.id = "Key" + key;
             }
 
             kTile.addEventListener("click", processKey);
 
-            if (key == "Enter"){
+            if (key == "Enter") {
                 kTile.classList.add("enterTile")
             }
-            else{
+            else {
                 kTile.classList.add("keyTile");
             }
             kbrow.appendChild(kTile);
@@ -94,19 +94,19 @@ function initialize() {
     }
 
     //populating
-    document.addEventListener("keyup", (e) => { 
+    document.addEventListener("keyup", (e) => {
         processInput(e);
     })
 
 }
 
-function processKey(){
-    let e = {"code" : this.id};
+function processKey() {
+    let e = { "code": this.id };
     processInput(e);
 }
 
 function processInput(e) {
-if (gameOver) return;
+    if (gameOver) return;
 
     if ("KeyA" <= e.code && e.code <= "KeyZ") {
         if (col < width) {
@@ -149,43 +149,44 @@ function update() {
     var guessWord = getGuess().toLocaleUpperCase();
     var validword = dictionary[guessWord.charAt(0)][0][guessWord];
     if (validword) {
-    let correct = 0;
+        let correct = 0;
 
-    let tempGuess = guess.split('');
+        let tempGuess = guess.split('');
 
-    let tempWord = word.split('');
+        let tempWord = word.split('');
 
-    for (let c = 0; c < width; c++) {
+        for (let c = 0; c < width; c++) {
 
-        let currTile = document.getElementById(row.toString() + '-' + c.toString());
-        let letter = currTile.innerText;
-        let letter2 = letter;
+            let currTile = document.getElementById(row.toString() + '-' + c.toString());
+            let letter = currTile.innerText;
+            let letter2 = letter;
 
-        //correct position
-        if (tempWord[c] == letter) {
-            currTile.classList.add("correct");
-            let kTile = document.getElementById("Key"+letter);
-            kTile.classList.remove("present");
-            kTile.classList.add("correct");
-            correct++;
-            tempWord[c] = " ";
-        }
+            //correct position
+            if (tempWord[c] == letter) {
+                currTile.classList.add("correct");
+                let kTile = document.getElementById("Key" + letter);
+                kTile.classList.remove("present");
+                kTile.classList.add("correct");
+                correct++;
+                tempWord[c] = " ";
+            }
 
-        // wrong position
-        else if (tempWord.includes(letter)) {
-            for (let i = 0; i < width; i++) {
-                if (tempWord[i] == letter) {
+            // wrong position
+            else if (tempWord.includes(letter)) {
+                for (let i = 0; i < width; i++) {
+                    if (tempWord[i] == letter) {
 
-                    if (tempWord[i] != tempGuess[i]) {    //lulat yggyy
-                        tempWord[i] = " ";
-                        letter = " ";
-                        currTile.classList.add("present");
-                        let kTile = document.getElementById("Key"+letter2);
-                        if ( !kTile.classList.contains("correct"))
-                        {
-                        kTile.classList.add("present");
+                        if (tempWord[i] != tempGuess[i]) {    //lulat yggyy
+                            tempWord[i] = " ";
+                            letter = " ";
+                            currTile.classList.add("present");
+                            let kTile = document.getElementById("Key" + letter2);
+                            if (!kTile.classList.contains("correct")) {
+                                kTile.classList.add("present");
+                            }
+
+
                         }
-                       
 
                     }
 
@@ -193,26 +194,24 @@ function update() {
 
             }
 
-        }
+            //absent
+            else {
+                currTile.classList.add("absent");
+                let kTile = document.getElementById("Key" + letter);
+                kTile.classList.add("absent");
+            }
 
-        //absent
-        else {
-            currTile.classList.add("absent");
-            let kTile = document.getElementById("Key"+letter);
-            kTile.classList.add("absent");
+            if (correct == width) {
+                gameOver = true;
+            }
         }
-
-        if (correct == width) {
-            gameOver = true;
-        }
+        row++;
+        col = 0;
+        document.getElementById("answer").innerText = "";
+    } else {
+        document.getElementById("answer").innerText = "OOPS! Word not Found. Try again";
+        document.getElementById("answer").classList.add("error");
     }
-    row++;
-    col = 0;
-    document.getElementById("answer").innerText = "";
-} else {
-    document.getElementById("answer").innerText = "OOPS! Word not Found. Try again";
-    document.getElementById("answer").classList.add("error");
-}
 }
 
 function getGuess() {
